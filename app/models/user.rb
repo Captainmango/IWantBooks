9e1ca1619332653c7a4e726db458272051d75ffc
Edward Heaver <edward.heaver01@gmail.com>
2020-03-07 16:29:46 +0000

    Writing migrations and validations for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :books
+    has_many :books, through: :lists
+    has_secure_password
+    validates :username, uniqueness: true
+    validates :first_name, presence: {message: "Please enter your first name"}
+    validates :last_name, presence: {message: "Please enter your surname"}
 end
--- db/migrate/20200307144856_create_lists.rb
+++ db/migrate/20200307144856_create_lists.rb
@@ -1,6 +1,10 @@
 class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
+      t.integer :book_id
+      t.integer :user_id
+      t.integer :name
+      t.integer :description
 
       t.timestamps
     end
