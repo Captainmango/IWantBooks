6a5308af5f31ed2d93ea28c8bfc88f757b0211f4
Edward Heaver <edward.heaver01@gmail.com>
2020-03-08 17:04:40 +0000

    Finished navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :comments
   resources :books
-  resources :users
+  
 
   root to: "home#page"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
