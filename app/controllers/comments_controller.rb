a2012e67651064fc2c130492cf05cfdc64325df3
Edward Heaver <edward.heaver01@gmail.com>
2020-03-13 22:59:18 +0000

    added partial to main view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,29 @@
 class CommentsController < ApplicationController
+
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
+
+    def show
+    end
+    
+    private
+
+    def comments_params
+        params.require(:book).permit(:title, :author, :self_link, :preview_link)
+    end
 end
