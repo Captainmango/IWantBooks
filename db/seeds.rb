646e18a1e1db3d939b98950b0c12e2a3ba7a2448
Edward Heaver <edward.heaver01@gmail.com>
2020-03-14 19:12:08 +0000

    Changes to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ user_1 = User.find(1)
 
 books = Book.search_books("Ruby")
 books["items"].each do |book|
-    new_book = user_1.books.create(:tag => book["id"],
+    new_book = user_1.books.create(
         :self_link => book["selfLink"],
         :title => book["volumeInfo"]["title"], 
         :author => book["volumeInfo"]["authors"], 
