02734ce888b97a32b716ff33a93a582665a35e65
Edward Heaver <edward.heaver01@gmail.com>
2020-03-14 12:03:37 +0000

    Created new and edit views for comments and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { :omniauth_callbacks => "users/omniauth_callbacks" }
+ 
+  resources :books do
+    resources :comments, only: [:index, :new, :edit, :show]
+  end
+
   resources :comments
-  resources :books
 
   get "/search", to: "search#results"
-
   
-  
-
   root to: "home#page"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
