954ee099bb8869e6b0ecc11ded5e808f548ce4c4
Edward Heaver <edward.heaver01@gmail.com>
2020-03-25 11:38:15 +0000

    Removed validation on book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
     has_many :comments
     has_many :users, through: :comments
 
-    validates :title,{ presence: true, uniqueness: true}
+    validates :title, presence: true
     validates :self_link,{ presence: true, uniqueness: true}
     validates :author, presence: true
     validates :preview_link, presence: true
